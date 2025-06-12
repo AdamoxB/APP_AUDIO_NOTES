458a21a162967d0308bfb25058043d05dafa22d9
Adam <bielaa3@gmail.com>
2025-06-12 11:26:23 +0200

    aktualizacje

--- APP_AUDIO_NOTES+MP3_CONVERER.py
+++ APP_AUDIO_NOTES+MP3_CONVERER.py
@@ -2,6 +2,7 @@ from io import BytesIO
 import streamlit as st
 from audiorecorder import audiorecorder  # type: ignore
 from dotenv import dotenv_values
+from hashlib import md5
 from openai import OpenAI
 
 ##############
@@ -41,6 +42,10 @@ def transcribe_audio(audio_bytes):
 
 # st.set_page_config(page_title="Audio Notatki", layout="centered")
 
+# Session state initialization
+if "note_audio_bytes_md5" not in st.session_state:
+    st.session_state["note_audio_bytes_md5"] = None
+
 if "note_audio_bytes" not in st.session_state:
     st.session_state["note_audio_bytes"] = None
 
@@ -56,14 +61,16 @@ if note_audio:
     audio = BytesIO()
     note_audio.export(audio, format="mp3")
     st.session_state["note_audio_bytes"] = audio.getvalue()
+
+    current_md5 = md5(st.session_state["note_audio_bytes"]).hexdigest()
+    if st.session_state["note_audio_bytes_md5"] != current_md5:
+        st.session_state["note_audio_text"] = ""
+        st.session_state["note_audio_bytes_md5"] = current_md5
+
     st.audio(st.session_state["note_audio_bytes"], format="audio/mp3")
 
     if st.button("Transkrybuj audio"):
         st.session_state["note_audio_text"] = transcribe_audio(st.session_state["note_audio_bytes"])
 
     if st.session_state["note_audio_text"]:
-        st.text_area(
-            "Transkrypcja audio",
-            value=st.session_state["note_audio_text"],
-            disabled=True,
-        )
+        st.text_area("Edytuj notatkę", value=st.session_state["note_audio_text"])
