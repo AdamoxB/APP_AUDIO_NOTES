085541814f85b75348d094aee4596fafedfe9258
Adam <bielaa3@gmail.com>
2025-06-12 10:37:27 +0200

    aktualizacje

--- APP_AUDIO_NOTES+MP3_CONVERER.py
+++ APP_AUDIO_NOTES+MP3_CONVERER.py
@@ -6,17 +6,17 @@ from io import BytesIO
 st.set_page_config(page_title="Audio Notatki", layout="centered")
 
 st.title("Audio Notatki")
-note_audio = audiorecorder(
-    start_prompt="Nagraj notatkę",
-    stop_prompt="Zatrzymaj nagrywanie",
-)
+# note_audio = audiorecorder(
+#     start_prompt="Nagraj notatkę",
+#     stop_prompt="Zatrzymaj nagrywanie",
+# )
 
-# Debugowanie
-if note_audio:
-    st.success("Nagrywanie zakończone.")
-    audio = BytesIO()
-    note_audio.export(audio, format="mp3")
-    note_audio_bytes = audio.getvalue()
-    st.audio(note_audio_bytes, format="audio/mp3")
-else:
-    st.warning("Naciśnij przycisk, aby nagrać.")
+# # Debugowanie
+# if note_audio:
+#     st.success("Nagrywanie zakończone.")
+#     audio = BytesIO()
+#     note_audio.export(audio, format="mp3")
+#     note_audio_bytes = audio.getvalue()
+#     st.audio(note_audio_bytes, format="audio/mp3")
+# else:
+#     st.warning("Naciśnij przycisk, aby nagrać.")
